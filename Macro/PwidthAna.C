125c2abc22023f2ee72d90f435ef645c153c00ad
XiaoYuSiang <s411930107202018@gmail.com>
2023-06-07 02:23:30 +0800

    debug: Pwidth Ana / EventAna for board id no from 0

--- Macro/PwidthAna.C
+++ Macro/PwidthAna.C
@@ -48,6 +48,7 @@ void PwidthAnaV2(const bool*OperMode, const int itcntgap=28) {
   defstyle();
   
   bool testMode        =true;  testMode         = OperMode[0];
+  // cout<<"testMode"<<"\t"<<testMode<<endl; throw;
   bool normalizeMode   =true;  normalizeMode    = OperMode[1];
   bool NegHiRatChMode  =true;  NegHiRatChMode   = OperMode[2];
   bool lowstatisticMode=false; lowstatisticMode = OperMode[3];
@@ -136,20 +137,20 @@ void PwidthAnaV2(const bool*OperMode, const int itcntgap=28) {
   TH1F *hPw[NumBD*NumCh]={},*hPwSE[NumBD*NumCh]={};
   TH1F *hRt[NumBD*NumCh]={};
   // TF1  *fPw[64]={},*fPwSE[64]={};
-  int staBID = MTBSysMaxBID*NumCh, endBID = NumBD*NumCh;/*@@@*/
+  int staBID = 128*NumCh, endBID = NumBD*NumCh;
   for(int iBD=0; iBD<NumBD; iBD++){
     int gidtmp = (BD[iBD]-1)*NumCh;
-    // cout<<iBD<<"\t"<<BD[iBD]<<"\t"<<gidtmp <<endl;
+    cout<<iBD<<"\t"<<BD[iBD]<<"\t"<<gidtmp <<endl;
     if(gidtmp<staBID) staBID = gidtmp;
     if(gidtmp>endBID) endBID = gidtmp+NumCh;
   }
-  int CapBID = endBID-staBID;
-  cout<<"Start:End:Capacity of BID\t"<<staBID<<"\t"<<endBID<<"\t"<<CapBID<<endl; //throw;
-  TH2F *hchPw   = new TH2F("hchPw","hchPw",binpw,0,binpw,CapBID,staBID,endBID);//ch64, pw: 0~180//@@
+  int CavBID = endBID-staBID;
+  cout<<staBID<<"\t"<<endBID<<"\t"<<CavBID<<endl; //throw;
+  TH2F *hchPw   = new TH2F("hchPw","hchPw",binpw,0,binpw,CavBID,staBID,endBID);//ch64, pw: 0~180//@@
   hchPw->GetYaxis()->SetRangeUser(staBID-1,endBID+1);
-  TH2F *hchPwSE = new TH2F("hchPwSE","hchPwSE",binpw,0,binpw,CapBID,staBID,endBID);//ch64, pw: 0~180
+  TH2F *hchPwSE = new TH2F("hchPwSE","hchPwSE",binpw,0,binpw,CavBID,staBID,endBID);//ch64, pw: 0~180
   TH2F *hchPwDS[2] = {hchPw,hchPwSE};//ch64, pw: 0~180
-  TH2F *hchRt = new TH2F("hchRt","hchRt",binrt,Starttime,Endtime,CapBID,staBID,endBID);//ch64, pw: 0~180
+  TH2F *hchRt = new TH2F("hchRt","hchRt",binrt,Starttime,Endtime,CavBID,staBID,endBID);//ch64, pw: 0~180
   cpwch->SetPhi(-30);
   cpwch->SetTheta(70);
   cpwchSE->SetPhi(-30);
@@ -164,7 +165,7 @@ void PwidthAnaV2(const bool*OperMode, const int itcntgap=28) {
     else tR->Draw("(channel+(board-1)*16):pwidth/pwidthScaleFactor>>hchPwSE",Form("IDtight>=4&&%s",SGTR),"colz");
     cpwch->cd();
 
-    t->Draw("(channel+(board-1)*16):pwidth>>hchPw",Form("%s",SGTR),"colz");//PS: no pwidthScaleFactor in *mu.root
+    t->Draw(Form("(channel+(board-1)*16):pwidth>>hchPw"),Form("%s",SGTR),"colz");//PS: no pwidthScaleFactor in *mu.root
     
     //cpwch->Update();
     char devoteCondition[40]={};
@@ -175,18 +176,21 @@ void PwidthAnaV2(const bool*OperMode, const int itcntgap=28) {
       if(normalizeMode){
         for(int ibd=0;ibd<NumBD;ibd++){
           for(int ich=0;ich<NumCh;ich++){
-
-            int Tmp_CHGID = (BDcheck(BD[ibd]))*NumCh+ich;
+            //(BD[ibd]-1)*NumCh+ich
+            int Tmp_CHGID = hchPwDS[i]->GetYaxis()->FindBin((BD[ibd]-1)*NumCh+ich);
+            // float binSta = 
+            // float binEnd = 
             // cout<<"Doing the normalization: ChGID: " <<Tmp_CHGID<<endl;
-            double ChIntegralNum  = hchPwDS[i]->Integral(-1,-1,Tmp_CHGID+1,Tmp_CHGID+1);
+            double ChIntegralNum  = hchPwDS[i]->Integral(-1,-1,Tmp_CHGID,Tmp_CHGID);
             outPwData<<BD[ibd]<<"\t"<<ich<<"\t"<<int(ChIntegralNum);
 
+            // cout<<(BD[ibd]-1)*NumCh+ich<<"\t"<<ibd<<"\t"<<ich<<"\t"<<Tmp_CHGID<<"\t"<<ChIntegralNum<<endl;
             for(int ibinpw=0;ibinpw<binpw;ibinpw++){
-              double ChOriginNum =hchPwDS[i]->Integral(ibinpw+1,ibinpw+1,Tmp_CHGID+1,Tmp_CHGID+1);
+              double ChOriginNum =hchPwDS[i]->Integral(ibinpw+1,ibinpw+1,Tmp_CHGID,Tmp_CHGID);
               double interData = 0;
               if(ChOriginNum==0) interData=0;
               else interData = ChOriginNum/ChIntegralNum;
-              hchPwDS[i]->SetBinContent(ibinpw+1,Tmp_CHGID+1,interData);
+              hchPwDS[i]->SetBinContent(ibinpw+1,Tmp_CHGID,interData);
               outPwData<<"\t"<<interData;
               // cout<<ibinpw<<"    "<<Tmp_CHGID+1<<"    "<<ChOriginNum/ChIntegralNum<<endl;
               // cout<<ibinpw<<"    "<<Tmp_CHGID+1<<"    "<<hchPw->GetBinContent(2*ibinpw+2,Tmp_CHGID+1+1)<<endl;
@@ -336,12 +340,14 @@ void PwidthAnaV2(const bool*OperMode, const int itcntgap=28) {
       hRt[ich+ibd*NumCh]  = new TH1F(hrtname,hrtname,binrt,Starttime,Endtime);
 
       for(int ibinpw=1; ibinpw<binpw+1;ibinpw++){
-        float TmpPwV0= hchPwDS[0]->GetBinContent(ibinpw,BDcheck(BD[ibd])*NumCh+ich+1),
-              TmpPwV1= hchPwDS[1]->GetBinContent(ibinpw,BDcheck(BD[ibd])*NumCh+ich+1);
+        // float TmpPwV0= hchPwDS[0]->GetBinContent(ibinpw,BDcheck(BD[ibd])*NumCh+ich+1),
+        //       TmpPwV1= hchPwDS[1]->GetBinContent(ibinpw,BDcheck(BD[ibd])*NumCh+ich+1);
+        float TmpPwV0= hchPwDS[0]->GetBinContent(ibinpw,hchPwDS[0]->GetYaxis()->FindBin((BD[ibd]-1)*NumCh+ich)),
+              TmpPwV1= hchPwDS[1]->GetBinContent(ibinpw,hchPwDS[1]->GetYaxis()->FindBin((BD[ibd]-1)*NumCh+ich));
         // cout<<BD[ibd]<<"\t"<<ich<<"\t"<<ibinpw<<"\t"<<BDcheck(BD[ibd])*NumCh+ich+1<<"\t"<<TmpPwV0<<"\t"<<TmpPwV1<<endl;
         hPw[ich+ibd*NumCh]->SetBinContent(ibinpw,TmpPwV0);
         hPwSE[ich+ibd*NumCh]->SetBinContent(ibinpw,TmpPwV1);
-      }
+      } //throw;
       /*
             for(int ibinrt=1; ibinrt<binrt+1;ibinrt++){
         RtValue[1] = 1.0*(hchRt->GetBinContent(ibinrt,(BD[ibd]-1)*NumCh+ich+1));
@@ -835,7 +841,7 @@ void PwidthAnaV2P(const bool*OperMode, const int itcntgap=28) {
   cALL->cd(2);
   cNTSA->DrawClonePad();
   DrawPdfPng(cALL,Form("%sHitNumAllSelect",DirResultGPw));
-
+  
   
   
   optime.Stop();
